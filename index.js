40c52e214437f13a6e1a1156a1ce0a07dc28da5f
ronmattss <ronmattss@gmail.com>
2024-07-26 08:22:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -147,20 +147,20 @@ client.on('messageCreate', message => {
     }
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+// client.on('interactionCreate', async interaction => {
+//     if (!interaction.isCommand()) return;
 
-    const command = client.commands.get(interaction.commandName);
+//     const command = client.commands.get(interaction.commandName);
 
-    if (!command) return;
+//     if (!command) return;
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-});
+//     try {
+//         await command.execute(interaction);
+//     } catch (error) {
+//         console.error(error);
+//         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+//     }
+// });
 
 client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
