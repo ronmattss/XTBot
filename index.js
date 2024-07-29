c6d18995be2030e1b6f04b45a4a984550e3a904f
ronmattss <ronmattss@gmail.com>
2024-07-29 11:55:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -181,10 +181,15 @@ client.on('interactionCreate', async interaction => {
   if (!command) return;
 
   try {
+      console.log(`Attempting to execute ${interaction.commandName}`);
       await command.execute(interaction);
   } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      console.error(`Error executing ${interaction.commandName}`, error);
+      if (interaction.replied || interaction.deferred) {
+          await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+      } else {
+          await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
   }
 });
 
