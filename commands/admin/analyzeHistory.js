48f8b13c123d9f14c7af01557affef2df24e3f5a
ronmattss <ronmattss@gmail.com>
2024-07-29 16:08:45 -0400

    Update analyzeHistory.js

--- commands/admin/analyzeHistory.js
+++ commands/admin/analyzeHistory.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, Collection } = require('discord.js');
 const wordLeaderboard = require('./wordLeaderboard');
-const wait = require('node:timers/promises').setTimeout;
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('analyzehistory')
@@ -21,17 +21,16 @@ module.exports = {
         await interaction.editReply("Waiting for messages");
 
         analyzeMessages(fetchedMessages);
-        
+        await wait(4_000);
         await interaction.followUp({ content: `Analyzed past messages for the word: "${wordLeaderboard.getWordToTrack()}" in channel ${channelId}`, ephemeral: true });
     },
 };
-
 
 async function fetchMessages(channel) {
     const fetchedMessages = new Collection();
     let lastId = null;
     const limit = 100;
-    let totalFetched = 1;
+    let totalFetched = 0;
     let whileLimit = 0;
 
     console.log("Fetching Messages");
@@ -43,23 +42,18 @@ async function fetchMessages(channel) {
         }
 
         const messages = await channel.messages.fetch(options);
-        whileLimit++;
-        console.log(`:${whileLimit} Fetching ${messages.size} messages`);
+        console.log(`${whileLimit} Fetched ${messages.size} messages`);
 
-        if (messages.size === 0 || totalFetched >= 100) {
+        if (messages.size === 0) {
             break;
         }
+        whileLimit++;
 
         messages.forEach(msg => fetchedMessages.set(msg.id, msg));
         lastId = messages.last().id;
-        totalFetched += messages.size;
     }
 
     console.log(`Total fetched messages: ${fetchedMessages.size}`);
-
-    // Save messages to a JSON file
-    saveMessagesToFile(fetchedMessages);
-
     return fetchedMessages;
 }
 
@@ -72,21 +66,4 @@ function analyzeMessages(messages) {
             wordLeaderboard.updateLeaderboard(message.author.id);
         }
     });
-}
-
-function saveMessagesToFile(messages) {
-    const messagesArray = Array.from(messages.values()).map(msg => ({
-        id: msg.id,
-        content: msg.content,
-        author: {
-            id: msg.author.id,
-            username: msg.author.username,
-            discriminator: msg.author.discriminator
-        },
-        createdAt: msg.createdAt
-    }));
-
-    const filePath = path.join(__dirname, 'json', 'messages.json');
-    fs.writeFileSync(filePath, JSON.stringify(messagesArray, null, 2), 'utf8');
-    console.log(`Messages saved to ${filePath}`);
 }
